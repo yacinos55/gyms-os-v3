c5d4018a02fc6da2bdca2a9498aac1ca0e868e67
yacinos55 <abdouahmedyacine@gmail.com>
2026-05-16 19:47:04 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/App.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
